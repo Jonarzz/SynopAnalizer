11b40d3be68a6e6215503ba8ef7d5ed8f6bab172
marecki90 <0marecki@gmail.com>
2015-05-23 20:17:12 +0200

    Update Constants.java

--- SynopAnalizer/src/com/nwpi/Constants.java
+++ SynopAnalizer/src/com/nwpi/Constants.java
@@ -3,7 +3,7 @@ package com.nwpi;
 public class Constants {
 
 	public static final char UNKNOWN_CHAR = '/';
-	public static final String UNKNOWN_STRING = '//';
+	public static final String UNKNOWN_STRING = "//";
 
 	public static final String CHAPTER_3_CODE = "333";
 	public static final String CHAPTER_4_CODE = "444";
